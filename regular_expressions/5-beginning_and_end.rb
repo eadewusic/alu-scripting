c22d90f493706b1d33f3d9f415e973478a30431d
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-09-08 20:03:44 -0700

    5. Not quite HBTN yet

---
+++ regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+# Check if an argument was provided
+if ARGV.empty?
+  puts "Usage: ./5-beginning_and_end.rb <input_string>"
+  exit(1)
+end
+
+# Get the input string from the command line argument
+input_string = ARGV[0]
+
+# Define the regular expression pattern
+pattern = /^h.n$/
+
+# Check if the input string matches the pattern
+if pattern.match(input_string)
+  puts input_string
+end
