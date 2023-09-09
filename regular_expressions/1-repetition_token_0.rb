d19a08ffb4f35d93990f4fa5de33f7a2ff5beffd
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-09-08 19:16:37 -0700

    1. Repetition Token #0

---
+++ regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+# Check if an argument was provided
+if ARGV.empty?
+  puts "Usage: ./1-repetition_token_0.rb <input_string>"
+  exit(1)
+end
+
+# Get the input string from the command line argument
+input_string = ARGV[0]
+
+# Define the regular expression pattern
+pattern = /hbtn+n/
+
+# Use the regular expression to find matches in the input string
+matches = input_string.scan(pattern)
+
+# Join and print the matched text
+puts matches.join
