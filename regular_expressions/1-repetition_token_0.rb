5a0e943d7fd82ced9707082f023814393defa997
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-09-09 04:32:28 +0200

    Update 1-repetition_token_0.rb

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -10,7 +10,7 @@ end
 input_string = ARGV[0]
 
 # Use the regular expression pattern with a corrected quantifier
-matches = input_string.scan(/hbtn[tn]*/)
+matches = input_string.scan(/hb[t]*n/)
 
 # Join and print the matched text
 puts matches.join
