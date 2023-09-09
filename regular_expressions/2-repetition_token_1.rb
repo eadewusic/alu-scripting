95c727ccef8363a408efd663b1b8374eacd94e39
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-09-09 04:43:23 +0200

    Update 2-repetition_token_1.rb

--- regular_expressions/2-repetition_token_1.rb
+++ regular_expressions/2-repetition_token_1.rb
@@ -2,18 +2,15 @@
 
 # Check if an argument was provided
 if ARGV.empty?
-  puts "Usage: ./2-repetition_token_1.rb <input_string>"
+  puts "Usage: ./script_name.rb <input_string>"
   exit(1)
 end
 
 # Get the input string from the command line argument
 input_string = ARGV[0]
 
-# Define the regular expression pattern
-pattern = /hbtn+t*n/
-
-# Use the regular expression to find matches in the input string
-matches = input_string.scan(pattern)
+# Use the regular expression pattern to find matches
+matches = input_string.scan(/hb?[tn]+/)
 
 # Join and print the matched text
 puts matches.join
