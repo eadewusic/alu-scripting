a85825cc0155b2f8eaeadb09b6c4fe8a7f34c720
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-11-11 13:57:19 +0200

    0. How many subs?

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,3 +1,44 @@
 #!/usr/bin/python3
+"""
+0-subs
+"""
 
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers for a given subreddit.
+    If the subreddit is invalid, returns 0.
+    """
+    # Set a custom User-Agent to avoid Too Many Requests error
+    headers = {'User-Agent': 'my_user_agent'}
+
+    # Reddit API endpoint for subreddit information
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    try:
+        # Make the API request
+        response = requests.get(url, headers=headers)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = response.json()
+            # Extract and return the number of subscribers
+            return data['data']['subscribers']
+        elif response.status_code == 404:
+            # Invalid subreddit returns status code 404
+            return 0
+        else:
+            # Handle other response codes if needed
+            print(f"Error: {response.status_code}")
+            return 0
+    except Exception as e:
+        print(f"Error: {e}")
+        return 0
+
+# Test the function
+if __name__ == '__main__':
+    subreddit = input("Enter a subreddit: ")
+    print(number_of_subscribers(subreddit))
 
