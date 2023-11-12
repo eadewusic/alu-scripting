bd0192dd279df38a0e4b10fe84d9f22111ba3f84
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-11-12 18:11:18 +0200

    Update 3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,99 +1,44 @@
 #!/usr/bin/python3
+'''a recursive function that queries the Reddit API,
+ parses the title of all hot articles, and prints a
+ sorted count of given keywords
 '''
-Recursively queries the Reddit API, parses the title of all hot articles,
-and prints a sorted count of given keywords.
-'''
-
 import requests
 
+
 def count_words(subreddit, word_list, fullname="", count=0, hash_table={}):
+    '''fetches all hot posts in a subreddit
+    Return:
+        None - if subreddit is invalid
     '''
-    Recursively queries the Reddit API, parses the title of all hot articles,
-    and prints a sorted count of given keywords.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-        word_list (list): A list of keywords to count.
-        fullname (str): The identifier for the last post in the previous page.
-        count (int): The total count of posts processed so far.
-        hash_table (dict): A dictionary to store the counts (default is {}).
-
-    Returns:
-        None
-    '''
-    # Check if the subreddit is None or not a string
     if subreddit is None or not isinstance(subreddit, str) or \
        word_list is None or word_list == []:
         return
-
-    # Base URL for the Reddit API
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-
-    # Parameters for the API request
     params = {'after': fullname, 'count': count}
-
-    # Custom User-Agent to avoid potential issues
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
-
-    # Make a GET request to the subreddit's hot.json endpoint
-    info = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
-    # Check if the request was successful (status code 200)
+    headers = {'user-agent': 'Mozilla/5.0 \
+(Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+    info = requests.get(url, headers=headers,
+                        params=params, allow_redirects=False)
     if info.status_code != 200:
         return None
-
-    # Parse the JSON response
     info_json = info.json()
-
-    # Extract and count the keywords in the titles
-    results = info_json.get('data', {}).get('children', [])
-    new_packet = [post.get('data', {}).get('title', '') for post in results]
-
+    results = info_json.get('data').get('children')
+    new_packet = [post.get('data').get('title') for post in results]
     for title in new_packet:
         for word in word_list:
             word = word.lower()
             formatted_title = title.lower().split(" ")
             if word in formatted_title:
-                if word in hash_table.keys():
+                if (word in hash_table.keys()):
                     hash_table[word] += formatted_title.count(word)
                 else:
                     hash_table[word] = formatted_title.count(word)
-
-    # Update variables for the next iteration
-    after = info_json.get('data', {}).get('after', None)
-    dist = info_json.get('data', {}).get('dist')
+    after = info_json.get('data').get('after', None)
+    dist = info_json.get('data').get('dist')
     count += dist
-
-    # Check if there are more pages (pagination) and recurse
     if after:
         count_words(subreddit, word_list, after, count, hash_table)
     else:
-        # Print the results after reaching the end
-        print_results(hash_table)
-
-def print_results(counts):
-    '''
-    Prints the sorted count of keywords.
-
-    Args:
-        counts (dict): A dictionary containing keyword counts.
-
-    Returns:
-        None
-    '''
-    # Sort the counts by value (descending) and then by key (ascending)
-    sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-
-    # Print the results
-    for keyword, count in sorted_counts:
-        print(f"{keyword}: {count}")
-
-# Example usage or testing code
-if __name__ == '__main__':
-    # Test the function with a subreddit and a list of keywords
-    subreddit_name = input("Enter a subreddit: ")
-    keywords_input = input("Enter a list of keywords separated by spaces: ")
-    keywords = [x for x in keywords_input.split()]
-
-    # Call the count_words function
-    count_words(subreddit_name, keywords)
+        {print('{}: {}'.format(key, value)) for
+         key, value in sorted(hash_table.items(), key=lambda i: (-i[1], i[0]))}
