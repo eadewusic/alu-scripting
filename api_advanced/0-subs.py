82784e0d6d1c7c70431a9129362e01a3402817e0
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-11-12 17:20:20 +0200

    PEP8 Validation resolved

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -18,16 +18,20 @@ def number_of_subscribers(subreddit):
     # Check if the subreddit is None or not a string
     if subreddit is None or not isinstance(subreddit, str):
         return(0)
+
     # Base URL for the Reddit API
     endpoint = 'https://www.reddit.com'
+
     # Custom User-Agent to avoid potential issues
     headers = {'user-agent': 'Mozilla/5.0 \
 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+
     # Make a GET request to the subreddit's about.json endpoint
     # allow_redirects=False to prevent automatic redirection
     info = requests.get('{}/r/{}/about.json'.format(
             endpoint,
             subreddit), headers=headers, allow_redirects=False)
+
     # Check if the request was successful (status code 200)
     if info.status_code == 200:
         # Parse the JSON response
@@ -35,5 +39,6 @@ def number_of_subscribers(subreddit):
         # Extract and return the number of subscribers from the response
         return(json_info.get('data').get('subscribers'))
     else:
-        # Return 0 for invalid subreddit or if there's an issue with the request
+        # Return 0 for invalid subreddit or if there's an
+        # issue with the request
         return(0)
